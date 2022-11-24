c8488e32f9e768e9be934cd20d0a862c3d395b96
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-11-24 11:24:04 +0000

    created dashboard component

--- danu-react-web-app/src/App.jsx
+++ danu-react-web-app/src/App.jsx
@@ -5,19 +5,27 @@ import LoginPage from './components/LoginPage'
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
 
+  //dummy login-function used to simulate login
   function loginFunction(){
     console.log("loggin in")
     setTimeout(()=>{
       setLoggedIn(prevValue => !prevValue)
       console.log("logged in")
-    },1000)
-    
+    },1000)   
   }
+
+  //main application function
+  function DanuDashboard() {
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+
   return (
     <div className="App">
       {!loggedIn ? <LoginPage
                         login={loginFunction}/> 
-      : console.log("logged in")}
+      : <DanuDashboard/>}
     </div>
   )
 }
--- danu-react-web-app/src/components/LoginPage.jsx
+++ danu-react-web-app/src/components/LoginPage.jsx
@@ -3,7 +3,7 @@ function LoginPage(props) {
     const {login} = props;
     return (
         <div className="login-page">
-            <img src="./public/danu.svg"/>
+            <img src="./danu.svg"/>
             <h1>Login Page</h1>
             <button onClick={login}>Login</button>
         </div>
