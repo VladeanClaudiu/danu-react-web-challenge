264af3edcec0523cbecd1b8ea8e9b0e836c2498a
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-11-25 10:55:26 +0000

    nivo imports

--- danu-react-web-app/src/components/AthleteOverview.jsx
+++ danu-react-web-app/src/components/AthleteOverview.jsx
@@ -1,7 +1,14 @@
+
+
 function AthleteOverview(props) {
     return (
         <div className="athlete-overview">
             <h1>Overview</h1>
+            <div>
+
+            </div>
+            
+        
         </div>
     )
 }
