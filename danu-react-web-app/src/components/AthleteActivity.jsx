2775823063e1ebd6cea3d15b5bcb9b48c53a99f1
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-11-27 16:38:31 +0000

    temp table added

--- danu-react-web-app/src/components/AthleteActivity.jsx
+++ danu-react-web-app/src/components/AthleteActivity.jsx
@@ -1,42 +1,40 @@
 import {DataGrid} from "@mui/x-data-grid"
 
+
+console.log(DataGrid)
 function AthleteActivity(props) {
     const columns = [
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
-        { field: 'age', headerName: 'Age', type: 'number', width: 90},
-        { field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
+        { field: 'fullName', headerName: 'Full name', width: 140 },
+        { field: 'date', headerName: 'Date', type: 'date', width: 100}, 
+        { field: 'test', headerName: 'Test', width: 100,},
+        { field: 'jumpHeight', headerName: 'Jump Height', width: 100,},
+        { field: 'topSpeed', headerName: 'Top Speed', width: 100,},
+        { field: 'endurance', headerName: 'Endurance', width: 100,}, 
       ];
 
       const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        { id: 1, fullName: 'Claudiu Vladean', date: "10.11.1995", test: "SQJ", jumpHeight: 92, topSpeed: 29, endurance: 4},
+        { id: 2, fullName: 'Claudiu Vladean', date: "10.11.1995", test: "SQJ", jumpHeight: 92, topSpeed: 29, endurance: 4},
+        { id: 3, fullName: 'Claudiu Vladean', date: "10.11.1995", test: "SQJ", jumpHeight: 92, topSpeed: 29, endurance: 4},
+        { id: 4, fullName: 'Claudiu Vladean', date: "10.11.1995", test: "SQJ", jumpHeight: 92, topSpeed: 29, endurance: 4},
+        { id: 5, fullName: 'Claudiu Vladean', date: "10.11.1995", test: "SQJ", jumpHeight: 92, topSpeed: 29, endurance: 4},
+        { id: 6, fullName: 'Claudiu Vladean', date: "10.11.1995", test: "SQJ", jumpHeight: 92, topSpeed: 29, endurance: 4},
+      
       ];
 
     return (
         <div className="athlete-activity">
             <h1>Activity</h1>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-            />
+            <div className="activity-table">
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                />
+            </div>
+            
         </div>
     )
 }
